13189e5876cf8297dc43ae82387e7bd29bd4cd31
willl85 <ngowilfried@gmail.com>
2023-03-07 15:41:05 +0100

    Add navigation component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 
 const App = () => {
+   
   const Shop = () => {
     return ( 
-      <div>
-        <h1>Shop page</h1>
-      </div>
-     );
+      <h1>Shop page</h1>
+    );
   }
 
   return (
     <Routes>
-      {/* Execute le composant Home si le lien se termine par / */}
-      <Route path="/" element={<Home />}>
+      {/* Execute les composants Navigation et Home si le lien se termine par / */}
+      <Route path="/" element={<Navigation />}>
+        {/* 
+          index (index={true ou false}) => afficher le composant lorsque le lien du parent est saisi 
+        */}
+        <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
