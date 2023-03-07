dac26966c8fd27c85f6bcd1e03c830b4010c64a3
willl85 <ngowilfried@gmail.com>
2023-03-07 16:12:37 +0100

    Add logo and customize nav-bar

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,17 @@
 import { Fragment } from "react";
 import { Outlet, Link } from 'react-router-dom';
 
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+
+import './navigation.styles.scss';
 
 const Navigation = () => {
 
     return (
       <Fragment>
         <div className="navigation">
-          <Link className="logo" to="/">
-            <div>Logo</div>
+          <Link className="logo-container" to="/">
+            <Logo className="logo" />
           </Link>
           <div className="nav-links-container">
             <Link className="nav-link" to="/shop">
