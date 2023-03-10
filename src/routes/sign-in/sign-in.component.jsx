4de1f474d0cc702c0ad31ed4fcb08d24dafdaf18
willl85 <ngowilfried@gmail.com>
2023-03-10 14:30:58 +0100

    Added our sign in component as well as firebase utils and brought in firebase libraries

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,11 +1,12 @@
 import {
   signInWithGooglePopup,
+  createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
     const logGoogleUser = async () => {
-        const response = await signInWithGooglePopup();
-        console.log(response);
+      const {user} = await signInWithGooglePopup();
+      createUserDocumentFromAuth(user);
     }
 
     return (
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,6 +7,13 @@ import { initializeApp } from 'firebase/app';
 /* Service authentification */ 
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
+/*
+    doc => permet de récuperer les documents de notre bdd firestar 
+    getDoc => récuperer les données du document
+    setDoc => modifier les données du document
+*/
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDh17X8wwuC8GSuzdGvkZmuM_hzJ_OX8ZU",
@@ -33,4 +40,60 @@ provider.setCustomParameters({
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
-/* Dire la manière dont ce fournisseur googleAuth doit se comporter */
+/*  
+    Creer (Instancié) la bdd ce qui nous permettra d'acceder à la bdd
+    de firebase 
+*/
+export const db = getFirestore();
+
+/*
+    Cette méthode asynchrone (car récupère les données externe(firebase)) reçoit un objet d'auth de l'utilisateur (userAuth)
+*/
+export const createUserDocumentFromAuth = async (userAuth) => {
+    // Cree une reference de document utilisateur
+  const userDocRef = doc(db, 'utilisateur', userAuth.uid);
+
+  console.log(userDocRef);
+
+  //  On récupère le document
+  //  await => car google doit le faire de manière asynchrone comme recupérer
+  // userDocRef;
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+
+  // Verifie si le doc exists sur la bdd
+  console.log(userSnapshot.exists()); // false => le document n'est pas encore dans la bdd
+
+  /*    Pseudo-code de la suite: 
+        if user data does not exist
+            create / set the document with the data from userAuth in my collection
+        if user data exist
+            return userDocRef
+  */
+
+  // Si les données de l'utilisateur n'existe pas
+  if (!userSnapshot.exists()) {
+    // Créer et definir le document 
+    const { displayName, email } = userAuth;
+
+    // Pour savoir quand l'utilisateur c'est connecté
+    const createdAt = new Date();
+
+    try {
+        /*
+            defini le document en lui rajoutant la reference de document utilisateur
+            puis les données avec lesquelles nous voulons installer
+        */
+        await setDoc(userDocRef, {
+            displayName,
+            email,
+            createdAt
+        });
+    } catch (error) {
+        // Lors de la création de l'ux nous allons enregistrer le message d'erreur
+        console.log('error creating the user', error.message);
+    }
+  } else {
+    return userDocRef;
+  }
+}
